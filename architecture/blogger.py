8785aa1f6ac74f2e40ef1fbf5eccc440510bf165
iceTheN00b <icethenoob3@gmail.com>
2024-01-14 05:57:39 -0800

    inception before first test

--- architecture/blogger.py
+++ architecture/blogger.py
@@ -18,9 +18,86 @@ class bloggerArchitecture:
 
         self.notepad: dict = {}
 
+        self.layer_2_modules = []
+
+        #layer 1
+        self.prior_module = self.define_prior_module()
+        self.executor_module = self.define_executor_module()
+        #self.publisher_module = self.define_publisher_module()
+
+        #layer 2
         self.idea_module = self.define_idea_module()
         self.research_module = self.define_research_module()
         self.outline_module = self.define_outline_module()
+
+    def define_prior_module(self):
+
+        def prior_module(input=""):
+
+            prior_module_subtask = """
+            0. conclude an idea using the idea_module
+            1. perform research using the research_module
+            """
+
+            prior_module_toolkit = [
+                Tool(
+                    name = "idea_module",
+                    func = self.idea_module,
+                    description="For concluding an the idea for a blogpost. There should be no input"
+                ),
+                Tool(
+                    name = "research_module",
+                    func = self.research_module,
+                    description="For performing research on a blogpost topic. Input is the blogpost topic"
+                )
+            ]
+
+            prior_module_agent = initialize_agent(
+                tools=prior_module_toolkit,
+                llm=self.soul,
+                agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                verbose=True
+            )
+            result = prior_module_agent.invoke({"input":prior_module_subtask})
+
+            return result
+
+        return prior_module
+
+    def define_executor_module(self):
+
+        def executor_module(input = ""):
+
+            executor_module_subtask = """
+            0. generate an outline using the outline_module
+            1. generate a blogpost using the writer_module
+            """
+
+            executor_module_toolkit = [
+                Tool(
+                    name = "outline_module",
+                    func = self.outline_module,
+                    description="For writing outline for blogposts. Input is the blogpost topic"
+                ),
+                Tool(
+                    name = "writer_module",
+                    func = self.writer_module,
+                    description="For writing blogposts. Input is the blogpost outline."
+                )
+            ]
+
+            executor_module_agent = initialize_agent(
+                tools=executor_module_toolkit,
+                llm=self.soul,
+                agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+                verbose=True
+            )
+            result = executor_module_agent.invoke({"input":executor_module_subtask})
+
+            return result
+
+
+        return executor_module
 
 
     def define_idea_module(self):
@@ -28,8 +105,8 @@ class bloggerArchitecture:
         #TODO: change this to check for old ideas first, before coming up with a new one.
 
         subtask = PromptTemplate.from_template(template="""
-        0. Search for relevant information pertaining to the latest advances in tech.
-        1. From this information, conclude what could be a compelling topic for a blogpost.
+        0. make use of search_  to look up "latest advances in tech"
+        1. make use of compell_ to conclude what could be a compelling topic for a blogpost.
         """)
 
         def search_(query =""):
@@ -47,7 +124,7 @@ class bloggerArchitecture:
             Tool(
                 name="search_",
                 func = search_,
-                description="to search for information. Input should be the query"
+                description="to search for information. Input should be the query."
             ),
 
             Tool(
@@ -65,7 +142,7 @@ class bloggerArchitecture:
                 #agent_kwargs={"prefix" :"Implement "},
                 verbose=True
             )
-            result = idea_agent.run(subtask)
+            result = idea_agent.invoke({"input":subtask})
 
             return result
 
@@ -73,14 +150,12 @@ class bloggerArchitecture:
 
     def define_research_module(self):
 
-         #takes note of things the agent has learnt.
-
         #TODO: implement conditional recursive searching ladies and gentlemen i.e make it such that the system continues to generate queries based until a satisfactory condition is met.
         #TODO: generally, i think iterative loops are neccessary
 
         subtask = PromptTemplate.from_template(template="""
-        0. Generate 3 queries that can be used to investigate the topic 
-        1. For each of these queries, search for information and take notes of the main facts.
+        0. use querier to generate queries that can be used to investigate the topic 
+        1. use noter to search for information for each of these queries
         """)
 
         def querier_(topic =""):
@@ -107,7 +182,7 @@ class bloggerArchitecture:
                 func = noter_,
                 description = "for when you need to research and then take notes of main facts and points. Input ought to query to search"
             )
-        ]                                     
+        ]
 
         def research_module(input =""):
             research_agent = initialize_agent(
@@ -125,16 +200,14 @@ class bloggerArchitecture:
 
     def define_outline_module(self):
         subtask = PromptTemplate.from_template("""
-        0. Develop an outline for the blogpost
+        0. use planner_ to define an outline for the blogpost
         """)
 
         def planner_(topic = ""):
             self.render.set_task()
-            #notes = json.dumps(self.notepad)
-            chain = LLMChain(llm = self.soul, prompt = PromptTemplate.from_template(f"precisely develop an outline for a blogpost"))
+            chain = LLMChain(llm = self.soul, prompt = PromptTemplate.from_template("precisely develop an outline for a blogpost based on {topic}")) #change this later to make use of information in notepad
             result = chain.run(topic)
             return result
-
 
         outline_module_tools = [
             Tool(
@@ -151,6 +224,6 @@ class bloggerArchitecture:
                 agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
                 verbose=True
             )
-            return outline_agent.run(subtask)
+            return outline_agent.invoke({"input":subtask})
 
         return outline_module
