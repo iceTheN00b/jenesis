817a47256ee2ef5ab98bffad2b1cdc888fb3f67d
iceTheN00b <icethenoob3@gmail.com>
2024-01-22 20:43:46 -0800

    configured renderer for communication with central server

--- agents/jenesis/jenesisRender.py
+++ agents/jenesis/jenesisRender.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 #this class is neccessary in order to have the agent's behaviour be visualizable for any rendering application, a feature i think will become
 #more common place were llm based agents turned towards consumers
@@ -6,13 +7,17 @@ import json
 class jenesisRender:
     def __init__(self):
         self.task = "IDLE"
-        self.DATA = "data/renderData/jenesis.json"
+        #self.DATA = "https://mobius.pythonanywhere.com/jenesis/render"
+        self.DATA = "https://127.0.0.1/jenesis/render"
 
     def set_task(self, task = "IDLE"):
         self.task = task
-        with open(self.DATA, "w") as render_data:
-            render_data.write(json.dumps({"task":f"{self.task}"},indent=2))
-        print(f"RENDER: {task}")
-
+        render_data = {
+            "task":self.task
+        }
+        try:
+            requests.get(self.DATA, render_data)
+        except:
+            print("unable to publish render data to central server")
 
 
